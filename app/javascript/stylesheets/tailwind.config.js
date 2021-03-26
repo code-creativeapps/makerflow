f0a9f6c2a0658c93875a975469c42fef82434165
Karim Morta <karim+code@creativeapps.studio>
2021-03-27 01:13:21 +0700

    Adds colors to purgecss whitelist

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -25,6 +25,34 @@ module.exports = {
       './app/javascript/**/*.js',
       './app/javascript/**/*.jsx',
     ],
+    options: {
+      safelist: [
+        'bg-red-500',
+        'bg-yellow-500',
+        'bg-green-500',
+        'bg-blue-500',
+        'bg-indigo-500',
+        'bg-purple-500',
+        'bg-pink-500',
+        'bg-black',
+        'text-red-500',
+        'text-yellow-500',
+        'text-green-500',
+        'text-blue-500',
+        'text-indigo-500',
+        'text-purple-500',
+        'text-pink-500',
+        'text-black',
+        'border-red-500',
+        'border-yellow-500',
+        'border-green-500',
+        'border-blue-500',
+        'border-indigo-500',
+        'border-purple-500',
+        'border-pink-500',
+        'border-black',
+      ]
+    }
   },
 
   // All the default values will be compiled unless they are overridden below
