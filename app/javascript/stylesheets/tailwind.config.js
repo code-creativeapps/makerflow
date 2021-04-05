8067fea1cf8d3ad897182e437a906ca518aa4132
Karim Morta <karim+code@creativeapps.studio>
2021-04-05 18:52:18 +0700

    Adds missing color to tailwind config

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -32,6 +32,7 @@ module.exports = {
         'bg-green-500',
         'bg-blue-500',
         'bg-indigo-500',
+        'bg-indigo-400',
         'bg-purple-500',
         'bg-pink-500',
         'bg-black',
